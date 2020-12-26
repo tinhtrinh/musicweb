ecc401f1b6bdeb5121ac1655f5c90fd99550a848
NothingCanStopMe <18521487@gm.uit.edu.vn>
2020-12-26 14:12:23 +0700

    Thêm iframe

---
+++ bxh/Ngay.js
@@ -0,0 +1,44 @@
+var songs = [
+    {
+        id: 1,
+        name_song: 'Perfect',
+        name_performer: 'Ed Sheeran',
+        image: "https://images.hungama.com/c/1/1d3/06e/23918543/23918543_300x300.jpg" 
+    },
+    {
+        id: 2,
+        name_song: 'Perfect',
+        name_performer: 'Ed Sheeran',
+        image: "https://images.hungama.com/c/1/1d3/06e/23918543/23918543_300x300.jpg" 
+    },
+    {
+        id: 3,
+        name_song: 'Perfect',
+        name_performer: 'Ed Sheeran',
+        image: "https://images.hungama.com/c/1/1d3/06e/23918543/23918543_300x300.jpg" 
+    },
+    {
+        id: 4,
+        name_song: 'Perfect',
+        name_performer: 'Ed Sheeran',
+        image: "https://images.hungama.com/c/1/1d3/06e/23918543/23918543_300x300.jpg" 
+    },
+    {
+        id: 5,
+        name_song: 'Perfect',
+        name_performer: 'Ed Sheeran',
+        image: "https://images.hungama.com/c/1/1d3/06e/23918543/23918543_300x300.jpg" 
+    },
+    {
+        id: 6,
+        name_song: 'Perfect',
+        name_performer: 'Ed Sheeran',
+        image: "https://images.hungama.com/c/1/1d3/06e/23918543/23918543_300x300.jpg" 
+    },
+    {
+        id: 7,
+        name_song: 'Perfect',
+        name_performer: 'Ed Sheeran',
+        image: "https://images.hungama.com/c/1/1d3/06e/23918543/23918543_300x300.jpg" 
+    }
+]
