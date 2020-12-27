203e91e7247d3d3a8c1fb54fae0397d8a253f2d4
Tuan Nguyen Anh <64674071+tuan908@users.noreply.github.com>
2020-12-27 23:01:45 +0700

    Add files via upload
    Add scroll to top button

--- project__country/Country.js
+++ project__country/Country.js
@@ -112,7 +112,20 @@ var arr = [
 $(document).ready(() => {
   $("#wrapper").html(
     arr.map(i => {
-      return '<div id="title"> <h1>' + i.title__left + '</h1> <p>' + i.title__right + '</p></div>'+'<div id="content__row"><div id="item"><img src="' + i.src1 + '"/><h4>' + i.title1 + '</h4> <p>' + i.author1 + '</p></div><div id="item"><img src="' + i.src2 + '"/><h4>' + i.title2 + '</h4> <p>' + i.author2 + '</p></div><div id="item"><img src="' + i.src3 + '"/><h4>' + i.title3 + '</h4> <p>' + i.author3 + '</p></div><div id="item"><img src="' + i.src4 + '"/><h4>' + i.title4 + '</h4> <p>' + i.author4 + '</p></div></div>';
+      return '<div id="title"> <h1>' + i.title__left + '</h1> <p>' + i.title__right + '</p></div>' + '<div id="content__row"><div id="item"><img src="' + i.src1 + '"/><h4>' + i.title1 + '</h4> <p>' + i.author1 + '</p></div><div id="item"><img src="' + i.src2 + '"/><h4>' + i.title2 + '</h4> <p>' + i.author2 + '</p></div><div id="item"><img src="' + i.src3 + '"/><h4>' + i.title3 + '</h4> <p>' + i.author3 + '</p></div><div id="item"><img src="' + i.src4 + '"/><h4>' + i.title4 + '</h4> <p>' + i.author4 + '</p></div></div>';
     })
   )
-});
+  $("#btn__backToTop").html(() => {
+    window.onscroll = () => {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $("#btn__backToTop").css("display", "block");
+      } else {
+        $("#btn__backToTop").css("display", "none");
+      }
+    }
+    $("#btn__backToTop").click(() => {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    });
+  })
+}) 
